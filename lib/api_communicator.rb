27489d114c3d5f177ca756cfa229965a3b781b88
tharveybrown <tharveybrown@gmail.com>
2020-05-04 15:56:09 -0700

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -8,7 +8,15 @@ def get_character_movies_from_api(character_name)
 
   response_string = RestClient.get('http://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
+  
+  results = response_hash["results"]
+  found_character = results.find do |result| 
+    result["name"] == character_name
+  end
 
+  films = found_character["films"]
+  character_films(films)
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,16 +28,37 @@ def get_character_movies_from_api(character_name)
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
+def character_films(film_apis)
+  film_collection = []
+  film_apis.map do |film|
+    response_string = RestClient.get(film)
+    response_hash = JSON.parse(response_string)
+    film_collection << response_hash
+  end
+  
+  film_collection
+end
+# [{"title"=>"A New Hope",
+#   "episode_id"=>4,
+#   "opening_crawl"=>
+#    "It is a period of c
 def print_movies(films)
+  films.map do |film|
+    puts film["title"]
+  end
   # some iteration magic and puts out the movies in a nice list
+
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
+
 end
+
 
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the star wars database"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  name = gets
+  name.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
 end
+
